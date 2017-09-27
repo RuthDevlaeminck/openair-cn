9499f5bb29277d556396d2b1aab3d92aac514d8c
Ruth Devlaeminck <ruth.devlaeminck@verizonwireless.com>
2017-09-27 23:58:18 +0000

    comment out this line per BJ, it causes a core dump

--- SRC/OAI_MME/oai_mme.c
+++ SRC/OAI_MME/oai_mme.c
@@ -75,7 +75,7 @@ main (
   pid_dir = bstr2cstr(mme_config.pid_dir, 1);
   pid_dir = pid_dir ? pid_dir : "/var/run";
   pid_file_name = get_exe_absolute_path(pid_dir);
-  bcstrfree(pid_dir);
+  //bcstrfree(pid_dir);
 
 #if DAEMONIZE
   pid_t pid, sid; // Our process ID and Session ID
